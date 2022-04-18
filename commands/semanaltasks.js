991f15e113f3d31c458b4af9cc0a1dec913b2ddf
Hallyson Nobres <hallysonnobres@gmail.com>
2022-04-17 21:21:58 -0400

    Classe Petiano criada

---
+++ commands/semanaltasks.js
@@ -0,0 +1,6 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Petiano = require("../petiano");
+
+petiano = new Petiano("Hallyson", "data");
+
+console.log(petiano);
--- petiano.js
+++ petiano.js
@@ -1,65 +1,48 @@
-class Petiano {
-    
-    constructor(user) {
-        this.user = user;
+class Petiano{
+    constructor(name, date){
+        this.name = name;
+        this.date = date;
+        this.toDo = [];
+        this.done = [];
+    }
+
+    setToDo(toDos){
+        this.toDo.push(toDos);
+    }
+
+    setDone(dones){
+        this.done.push(dones);
+    }
+
+    getPetiano(){
+        return {
+            "nome":`${this.name}`,
+            "ToDo":`${this.toDo}`,
+            "Done":`${this.done}`,
+            "Date":`${this.date}`
+        };
     }
 }
 
-class ToDo  {
-    constructor() {
-        
-        this.todo = []
-    }
-    newToDo(user, ...ToDo) {
-        let petiano = new Petiano(user)
-        this.todo.push({user,ToDo})
-        
+module.exports = Petiano;
 
-        return petiano
-    }
-    get allpetiano() {
-        return this.todo
 
-    }
-    get numberOfPetianos() {
-        return this.todo.length
-    }
+/* petianos = [];
+
+
+for(i = 0; i < 5; i++){
+    data = "18/04/2022";
+
+    petiano = new Petiano(message.user, message.date);
+
+    escreverTodo()
+    petiano.setToDo("Aulas da monitoria");
+    petiano.setToDo("Aulas da monitoria");
+
+    petiano.setDone("- Monitoria: Terminei a lista de ponteiros", "- Aulas da monitoria");
+
+    petianos.push(petiano.getPetiano());
+
 }
 
-class Done {
-    constructor() {
-        
-        this.done = []
-    }
-    newToDo(user, ...Done) {
-        let petiano = new Petiano(user)
-        this.done.push({user,Done})
-
-        return petiano
-    }
-    get allpetiano() {
-        return this.done
-
-    }
-    get numberOfPetianos() {
-        return this.done.length
-    }
-}
-
-
-let petianosToDo = new ToDo()
-let petianosDone = new Done()
-
-/*petianosToDo.newToDo("Hallyson N. Fernandes", "- Aulas da monitoria", "- Verificar lista e reunião com professor")
-petianosDone.newToDo("hallyson N. Fernandes", "- Monitoria: Terminei a lista de ponteiros", "- Aulas da monitoria")
-
-petianosToDo.newToDo("Jhénifer Matos", "- Bot Discord", "- estudar sobre WordPress para o site", "Reunião Sangue Bom")
-petianosDone.newToDo("Jhénifer Matos", "- participei da reunião do pet sangue bom pra definir a função de cada um", "- Estudei Wordpress")
-
-console.log(petianosToDo.allpetiano)
-
-console.log(petianosDone.allpetiano)*/
-
-
-
-
+*/
