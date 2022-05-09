532b8a090bc0ef0371e8d00a7d1450b01c34eaba
Hallyson Nobres <hallysonnobres@gmail.com>
2022-05-09 15:05:13 -0400

    parametro para receber data criado e npm atualizado

--- commands/semanaltasks.js
+++ commands/semanaltasks.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 //Main function
-function creatingDocument() {
+function creatingDocument(date) {
     const Petiano = require("../petiano");
     petiano = new Petiano("Hallyson", "data");
+    console.log(date);
 
     messagesFiltered = filteringMessages();
 }
@@ -16,12 +17,38 @@ function filteringMessages(){
     return messages;
 }
 
+//verify date format 
+async function verifyDate(interaction, date){
+    
+    try {
+        //pattern date
+        const regex_date = /\d{1,2}\D\d{1,2}\D\d{4}/g;
+
+        if(!regex_date.test(date))
+            throw new Error('formato de data inválido!');    
+        else
+            //call main function
+            await creatingDocument(date);
+
+        await interaction.reply('Documento atualizado!');
+
+    } catch (err) {
+        console.log(err);
+        await interaction.reply("Formato de data inválido");
+    }
+    
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('semanaltasks')
-        .setDescription('Le as mensagens até a data especificada e cria um documento'),
+        .setDescription('Le as mensagens até a data especificada e cria um documento')
+        .addStringOption(option =>
+            option.setName('data')
+                .setDescription('Data da última reunião no formato dd/mm/aaaa')
+                .setRequired(true)),
     async execute(interaction) {
-        await creatingDocument();
-        await interaction.reply('Documento atualizado!');
+        const date = interaction.options.getString('data');
+        await verifyDate(interaction, date);      
     }
 };
